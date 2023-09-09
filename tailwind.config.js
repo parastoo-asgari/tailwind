079015990e3685c3452523f83985e73ba6b77948
Parastoo-Asgari <135821750+parastoo-asgari@users.noreply.github.com>
2023-09-09 17:32:59 +0330

    project with tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import(‘tailwindcss’).Config} */
+module.exports = {
+content: ["./asset/**/*.{html,js}"],
+theme: {
+extend: {
+  backgroundImage: {
+    'parastoo': "url('asset/img/3-f.jpg')",
+    
+  }
+},
+},
+plugins: [],
+}
